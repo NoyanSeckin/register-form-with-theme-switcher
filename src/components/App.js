0252f69aa66f4a0585fecdb22639e363be830810
FKNoyan <egemennoyan@hotmail.com>
2022-04-11 22:02:03 +0300

    form layout finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
-import Form from "./Form";
+import SignupForm from "./Form";
 function App() {
   return (
     <div className="container">
-      <Form></Form>
+      <div className="col-6">
+        
+      </div>
+      <SignupForm></SignupForm>
     </div>
   );
 }
